fc9e1067ba3d2cf8bbcbc9040db752ef34caa425
Safaa Kadhim <66439492+safaaraed99@users.noreply.github.com>
2023-03-27 17:42:28 -0400

    update ComputerSystem.h
    I changed threads to p_thread and remove struct to include aircraftData.h

--- ATC_1/src/computerSystem.h
+++ ATC_1/src/computerSystem.h
@@ -2,70 +2,59 @@
 #define COMPUTERSYSTEM_H_
 #include <iostream>
 #include <vector>
-#include <thread>
+#include <pthread.h>
 #include <mutex> 
+#include <AircraftData.h>
+//h file radar
 using namespace std;
-
-struct Aircraft {
-
-    int idAircraft;
-    int position_x;
-    int position_y;
-    int position_z; 
-};
 
 
 class ComputerSystem {
-/*
-private:
-
-	//To calculate and detect Violations
-	void computeViolations();
-
-	//To display data on Display Data
-	void showDataDisplay();
-
-	//To update period
-	void updateAircraftPosition();
-
-	//To emit an alert in case a violation occurs
-	void sendViolationAlert();
-
-    void periodicComputations(); 
-
-    //Threads to detect alert to operator: 
-    std::thread detectViolations;
-
-    //Boolean function to stop detectViolation Thread: 
-    bool falseFlag; 
-
-    //Thread to protect shared data: 
-    std::thread aircraftMutex_;
-
-    std::vector<Aircraft> aircraft;
-
-
-
-    
 
 public:
 
-	//constructor:
-    //We need to know the number of aircrafts in the air to determine if any violations occur 
-	ComputerSystem(Radar& radar, int numberAircrafts,int periodicComputations);
-
-	//Destructor:
+    //constructor: We need to know the number of aircrafts in the air to determine if any violations occur 
+	ComputerSystem(Radar& radar, int numberAircrafts, int periodicComputations);
+	
+    //Destructor:
 	~ComputerSystem();
-
-    //To start the thread for periodic computations to detect violations
+    
+    //To start the pthread for periodic computations to detect violations
     void startPeriodicComputations(); 
-
-    //To stop the thread for periodic computations to detect violation
+    
+    //To stop the pthread for periodic computations to detect violation
     void stopPeriodicComputations(); 
 
-*/
+	//To calculate and detect Violations
+	void computeViolations();
+	
+    //To display data on Display Data
+	void showDataDisplay();
+	
+    //To update period
+	void updateAircraftPosition();
+	
+    //To emit an alert in case a violation occurs
+	void sendViolationAlert();
+   
+    //To calculate periodic computations
+    void periodicComputations(); 
+   
+    //To send updated Data to data display
+    void sendDataToDataDisplay(int ID,int x,int y,int z);
+
+    //pthread to detect alert to operator: 
+    pthread_t detectViolations;
+   
+    //Boolean function to stop detectViolation Thread: 
+    bool falseFlag; 
+   
+    //pthread to protect shared data: 
+    pthread_t aircraftMutex_;
+    vector<Aircraft> aircraft;
+    
+	
 
 };
 
 #endif
-
