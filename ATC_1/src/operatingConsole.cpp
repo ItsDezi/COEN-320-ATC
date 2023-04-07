cbc17b884f1152c3b17d0e6110bb3e6a08331e76
Arandeep Grewal <90730390+Arandeep24@users.noreply.github.com>
2023-04-07 17:25:33 -0400

    Update operatingConsole.cpp

--- ATC_1/src/operatingConsole.cpp
+++ ATC_1/src/operatingConsole.cpp
@@ -1,3 +1,4 @@
+
 #include "OperatingConsole.h"
 #include <iostream>
 #include <string>
@@ -6,9 +7,68 @@
 #include <unistd.h>
 #include "Client.cpp"
 
-void OperatingConsole::run() {
-    Command command;
 
+   void OperatingConsole::writeToFile( string fileName, vector<Command>) commands{
+  
+  
+    
+    // Write data to output file
+    ofstream oFile(fileName);
+     
+    //string commandReadOrChange;
+   
+    //string commandattributes;
+    
+    //string commandString;
+     string newValue;
+    
+    // Write to the file
+    for(int i=0;i<commands.size();i++){
+        
+        string commandReadorChange;
+        
+        if(commands[i].readOrChange == 0)
+            commandReadOrChange = "Read";
+        else if(commands[i].readOrChange == 1)
+            commandReadOrChange = "change";
+        else
+            commandReadOrChange = "Error no read nor change";
+            
+        string commandAttributes;
+        if(commands[i].attributes == 1)
+            commandAttributes = "x speed";
+        else if(commands[i].attributes == 2)
+            commandAttributes = "y speed";
+        else if(commands[i].attributes == 3)
+            commandAttributes = "z speed";
+        else
+            commandAttributes = "Error: unknown attribute";
+            
+        string commandString;    
+        if (commands[i].readOrChange == 0)
+        commandString = "ID: " + to_string(commands[i].ID) + ", Command: " + commandReadOrChange;
+     
+        else if (commands[i].readOrChange == 1)
+          commandString = "ID: " + to_string(commands[i].ID) + ", Command: " + commandReadOrChange + ", Attribute: " + commandAttributes + "new value: " + to_string(commands[i].newValue) ;
+        
+        
+        oFile <<commandString<< endl;
+    }
+    
+    // Close the file
+    oFile.close();
+
+    
+    
+    
+    // Write data to output file
+    //data.writeToFile("command.txt", vector<string> userString);
+   }
+    
+    void *OperatingConsole::run( void *arguments) {
+       
+     Command *commandPointer = (command *)arguments;
+     Command command = (*commandPointer);
     // Read input from the user
     cout << "Enter 0 to read object, 1 to change object: "<< endl;
     cin >> command.readOrChange;
@@ -19,24 +79,37 @@ void OperatingConsole::run() {
     cout << "3 - z\n";
     cin >> command.attributes;
 
-    cout << "Enter aircraft ID: "<< endl;
+    cout << "Enter aircraft ID: ";
     cin >> command.ID;
 
     if (command.readOrChange == 1) {
-        cout << "Enter new value: "<< endl;
+        cout << "Enter new value: "<<endl;
         cin >> command.newValue;
+        
+    pthread_exit(NULL);
+    return NULL;
     }
     
-    static void* computerSystem(void* args){
+    //static void* computerSystem(void* args){
         
-        MPData sendData;
-        
-    // Output the command
-   }
- }
+      void OperatingConsoleClient(MPData MsgToSend){
 
-void *OperatingConsole::runThread(void *arg) {
-    OperatingConsole *console = (OperatingConsole*)arg;
-    console->run();
-    return NULL;
-}
+		int server_coid; //server connection ID.  // change channel name 
+		if ((server_coid = name_open(MsgToSend.channelName.c_str(), 0)) == -1) {
+			perror("Error occurred while attaching the channel");
+		}
+
+		if (MsgSend(server_coid, &MsgToSend, sizeof(MsgToSend), NULL,0) == -1) {
+			printf("Error while sending the message from Client");
+		}
+
+	};
+
+   };
+   
+   #endif
+
+
+
+
+
